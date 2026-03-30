77818cffbc3af412cec6e1a049190b209d0ffc26
sowmyasri-32156 <sowmyasri-32156@users.noreply.github.com>
2026-03-30 20:34:12 +0530

    Add exp_1

---
+++ 24cseaiml012/experiments/exp_1/q.7.py
@@ -0,0 +1,7 @@
+a=int(input("enter marks of 1st subject:"))
+b=int(input("enter marks of 2nd subject:"))
+c=int(input("enter marks of 3rd subject:"))
+sum = a+b+c
+percentage=sum/3
+print(sum)
+print(percentage)
---
+++ 24cseaiml012/experiments/exp_1/q_.4.py
@@ -0,0 +1,6 @@
+a=int(input("enter a number:"))
+b=int(input("enter another number:"))
+sum=a+b
+print("sum:",sum)
+product=a*b
+print("product:",product)
